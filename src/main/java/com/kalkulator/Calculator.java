e1a014c7b41df6c0fcb8c3b56cb228a0b786fbae
Piotr Drzezdzon <jazzjohn78@gmail.com>
2023-01-14 20:54:11 +0100

    added methods

--- src/main/java/com/kalkulator/Calculator.java
+++ src/main/java/com/kalkulator/Calculator.java
@@ -1,4 +1,17 @@
 package com.kalkulator;
 
 public class Calculator {
+    public double add(double a, double b){
+        return a + b;
+    }
+
+    public double subtract(double a, double b){
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("2 + 3 = " + calc.add(2, 3));
+        System.out.println("4.5 - 1.75 = " + calc.subtract(4.5, 1.75));
+    }
 }
